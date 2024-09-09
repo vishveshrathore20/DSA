465f1fb505be536cb056d91e8da95317d81bc33e
vishveshrathore20 <vishveshrathore20@gmail.com>
2024-09-09 14:58:38 +0530

    factorial and swap

---
+++ factorial.java
@@ -0,0 +1,11 @@
+public class factorial {
+  public static void main(String[] args) {
+    int n = 5;
+    int c = 1;
+    for(int i=n;i>=1;i--){
+      c = c*i;
+    }
+    System.out.println(c);
+    
+  }
+}
---
+++ swap.java
@@ -0,0 +1,12 @@
+public class swap {
+  public static void main(String[] args) {
+    int a =5 ;
+    int b = 4;
+
+    a= a+b;
+    b= a-b;
+    a= a-b;
+    System.out.println("value of a " + a );
+    System.out.println("value of b " + b );
+  }
+}
