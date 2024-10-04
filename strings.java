226d77c8f123c0dabdf0d08e8f0808a8895af1bd
vishveshrathore20 <vishveshrathore20@gmail.com>
2024-10-04 15:20:23 +0530

    string immmutable

--- strings.java
+++ strings.java
@@ -3,5 +3,9 @@ class strings {
 
     public static void main(String[] args) {
 
+        String s2 = new String("Hello");
+        String s1 = s2.intern();
+        System.out.println(s1 == s2);
+
     }
 }
